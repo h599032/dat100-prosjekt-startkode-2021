4829c926cf0b7485eed38f3ba0b1dc0bcabac0ac
Susanne <Susanne@10.30.0.239>
2021-10-16 19:41:21 +0200

    .

--- src/no/hvl/dat100/prosjekt/kontroll/Bord.java
+++ src/no/hvl/dat100/prosjekt/kontroll/Bord.java
@@ -7,40 +7,40 @@ import no.hvl.dat100.prosjekt.kontroll.dommer.Regler;
 import no.hvl.dat100.prosjekt.modell.Kort;
 
 /**
- * Klasse som implementerer bordet som spilles p√•. 
+ * Klasse som implementerer bordet som spilles p√•.
  * 
- * Klassen har objektvariablene bunkeTil og bunkeFra som skal brukes til √• representere 
- * kortene som er i de to bunker p√• border. 
+ * Klassen har objektvariablene bunkeTil og bunkeFra som skal brukes til √•
+ * representere kortene som er i de to bunker p√• border.
  */
 public class Bord {
 
 	private KortSamling bunkeFra;
 	private KortSamling bunkeTil;
-	
+
 	/**
-	 * Metoden oppretter to bunker, til- og fra-bunken
-	 * Alle kortene legges til fra-bunken. 
+	 * Metoden oppretter to bunker, til- og fra-bunken Alle kortene legges til
+	 * fra-bunken.
 	 */
 	public Bord() {
-		
+
 		// TODO - START
-		
+
 		bunkeFra = new KortSamling();
 		bunkeFra.leggTilAlle();
 		bunkeTil = new KortSamling();
-		//throw new UnsupportedOperationException(TODO.constructor("Bord"));
+		// throw new UnsupportedOperationException(TODO.constructor("Bord"));
 		// TODO - END
 	}
-	
+
 	/**
 	 * Gir peker/referanse til til-bunken.
 	 * 
 	 * @return referanse/peker til til-bunken.
 	 */
 	public KortSamling getBunkeTil() {
-		
+
 		return bunkeTil;
-		
+
 	}
 
 	/**
@@ -49,18 +49,18 @@ public class Bord {
 	 * @return referanse/peker til fra-bunken.
 	 */
 	public KortSamling getBunkeFra() {
-		
+
 		return bunkeFra;
-		
+
 	}
-	
+
 	/**
 	 * Sjekker om til-bunken er tom.
 	 * 
 	 * @return true om til-bunken er tom, false ellers.
 	 */
 	public boolean bunketilTom() {
-		
+
 		// TODO - START
 		if (bunkeTil.getAntalKort() == 0) {
 			return true;
@@ -77,25 +77,25 @@ public class Bord {
 	 * @return true om fra-bunken er tom, false ellers.
 	 */
 	public boolean bunkefraTom() {
-		
+
 		// TODO - START
 		if (bunkeFra.getAntalKort() == 0) {
 			return true;
 		}
 		return false;
 		// throw new UnsupportedOperationException(TODO.method());
-	
+
 		// TODO - END
-		
+
 	}
-	
+
 	/**
 	 * Gir antall kort i fra-bunken.
 	 * 
 	 * @return antall kort i fra-bunken.
 	 */
 	public int antallBunkeFra() {
-		
+
 		// TODO - START
 		return bunkeFra.getAntalKort();
 		// throw new UnsupportedOperationException(TODO.method());
@@ -109,92 +109,93 @@ public class Bord {
 	 * @return antall kort i til-bunken.
 	 */
 	public int antallBunkeTil() {
-		
+
 		// TODO - START
 		return bunkeTil.getAntalKort();
 		// throw new UnsupportedOperationException(TODO.method());
 
 		// TODO - END
 	}
-	
+
 	/**
 	 * Tar ¯verste kortet fra fra-bunken og legger dettte til til-bunken (med
 	 * billedsiden opp, men det trenger ikke gruppen tenke pÂ).
 	 */
 	public void vendOversteFraBunke() {
-		
+
 		// TODO - START
-		Kort[]  samling = bunkeFra.getSamling();
-		Kort[] samling2 = bunkeTil.getSamling();
-		if (antallBunkeFra() < samling.length) {
-		samling[antallBunkeFra()] = samling2[antallBunkeTil()+1]; 
-		antallBunkeFra();
-		}
-		
-		
-		
+		Kort[] samling = bunkeFra.getSamling();
+		Kort k = samling[antallBunkeFra()-1];
+		bunkeTil.leggTil(k);
+		bunkeFra.fjern(k);
+
 		// throw new UnsupportedOperationException(TODO.method());
 		// TODO - END
-		
+
 	}
-		
+
 	/**
-	 * Tar √∏verste kortet fra fra-bunken.
+	 * Tar ¯verste kortet fra fra-bunken.
 	 * 
 	 * @return peker/referanse til det kort som blev tatt fra fra-bunken
 	 */
-	
+
 	public Kort taOversteFraBunke() {
-		
+
 		// TODO - START
-		
-		throw new UnsupportedOperationException(TODO.method());
+		return bunkeFra.taSiste();
+		 
+
+		// throw new UnsupportedOperationException(TODO.method());
 
 		// TODO - END
 	}
-	
+
 	/**
-	 * Metode som leser √∏verste kortet i til-bunken. Kortet vil fremdeles v√¶re
-	 * √∏verst i til-bunken etter at metoden er utf√∏rt.
+	 * Metode som leser ¯verste kortet i til-bunken. Kortet vil fremdeles vÊre
+	 * ¯verst i til-bunken etter at metoden er utf√∏rt.
 	 * 
-	 * @return peker/referanse til √∏verste kortet i til-bunken.
+	 * @return peker/referanse til ¯verste kortet i til-bunken.
 	 */
 	public Kort seOversteBunkeTil() {
-		
+
 		// TODO - START
-		
-		throw new UnsupportedOperationException(TODO.method());
+		return bunkeTil.seSiste();
+		// throw new UnsupportedOperationException(TODO.method());
 
 		// TODO - END
 	}
-	
+
 	/**
-	 * N√•r fra-bunken blir tom, tar man vare p√• kortet p√Ç toppen av til-bunken.
-	 * Deretter legges alle den andre kortene i til-bunken over i fra-bunken.
-	 * Denne stokkes og kortet som man har tatt vare p√Ç legges tilbake i
-	 * til-bunken. Det vil n√• v√¶re det eneste kortet i til-bunken.
+	 * NÂr fra-bunken blir tom, tar man vare pÂ kortet, pÂ toppen av til-bunken.
+	 * Deretter legges alle de andre kortene i til-bunken over i fra-bunken. Denne
+	 * stokkes og kortet som man har tatt vare pÂÇ legges tilbake i til-bunken. Det
+	 * vil nÂ vÊre det eneste kortet i til-bunken.
 	 */
 	public void snuTilBunken() {
 
 		// TODO - START
-		
+		for(int i = 0; i < antallBunkeTil()-1; i++) {
+		if (bunkeFra == null) {
+			
+		}
+		}
 		throw new UnsupportedOperationException(TODO.method());
 		// TODO - END
 	}
-		
+
 	/**
-	 * Metode som legger et kort i til-bunken. 
+	 * Metode som legger et kort i til-bunken.
 	 * 
-	 * @param k
-	 * 			kort som skal legges ned. 
-	 * 	
+	 * @param k kort som skal legges ned.
+	 * 
 	 */
 	public void leggNedBunkeTil(Kort k) {
-		
+
 		// TODO - START
-		
-		throw new UnsupportedOperationException(TODO.method());
+		bunkeTil.leggTil(k);
+		// throw new UnsupportedOperationException(TODO.method());
 		// TODO - END
-				
+
 	}
 }
